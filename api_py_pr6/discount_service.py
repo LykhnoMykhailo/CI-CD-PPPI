2633069b672867dfb75e75ad7ba6867f4eb17300
Avtoritetik-ukr <mkarmaza184@gmail.com>
2025-10-27 20:13:15 +0200

    update
    Заміненно магічні числа

--- api_py_pr6/discount_service.py
+++ api_py_pr6/discount_service.py
@@ -1,7 +1,11 @@
 def calculate_discount(original_price: float, discount_percent: float) -> float:
-    if not (0 <= discount_percent <= 100):
+    min = 0
+    max = 100
+    half = 2
+    if not (min <= discount_percent <= max):
         # Використовуємо ValueError для недійсних вхідних даних
         raise ValueError("Відсоток знижки має бути між 0 і 100.")
 
-    discount_amount = original_price * (discount_percent / 100.0)
-    return round(original_price - discount_amount, 2)
+    discount_amount = original_price * (discount_percent / max)
+    return round(original_price - discount_amount, half)
+calculate_discount(200, 15)
