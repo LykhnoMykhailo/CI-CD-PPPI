4d44d9c924a81be97679755ef80355c9d6194a58
kslyr <st9252076@stud.duikt.edu.ua>
2025-10-28 12:29:36 +0200

    Update

--- api_py_pr6/discount_service.py
+++ api_py_pr6/discount_service.py
@@ -1,10 +1,11 @@
-def calculate_discount(original_price: float, discount_percent: float) -> float:
-    min = 0
-    max = 100
-    half = 2
-    if not (min <= discount_percent <= max):
-        # Використовуємо ValueError для недійсних вхідних даних
+def calculate_discount(original_price: float, discount_percentage: float) -> float:
+    MIN_PERCENT = 0
+    MAX_PERCENT = 100
+    DECIMAL_PLACES = 2
+
+    if not (MIN_PERCENT <= discount_percentage <= MAX_PERCENT):
         raise ValueError("Відсоток знижки має бути між 0 і 100.")
 
-    discount_amount = original_price * (discount_percent / max)
-    return round(original_price - discount_amount, half)
+    discount_amount = original_price * (discount_percentage / MAX_PERCENT)
+
+    return round(original_price - discount_amount, DECIMAL_PLACES)
